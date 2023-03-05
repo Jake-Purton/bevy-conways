9839593961fe7c0582ab7940153ebd934e1b4b8a
Jake-Purton <jakecapurton@gmail.com>
2023-03-05 21:16:57 +0000

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-use bevy::{prelude::*, render::{render_resource::{Extent3d, TextureDimension, TextureFormat, TextureUsages}}, asset::HandleId};
+use bevy::{prelude::*, render::{render_resource::{Extent3d, TextureDimension, TextureFormat, TextureUsages}, texture::ImageSampler}, asset::HandleId};
+use rand::Rng;
 
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             window: WindowDescriptor {
-                width: CONWAYS_MAP_SIZE.x,
-                height: CONWAYS_MAP_SIZE.y,
+                width: CONWAYS_SCREEN_SIZE.x,
+                height: CONWAYS_SCREEN_SIZE.y,
                 title: "To do".to_string(),
                 resizable: true,
                 ..Default::default()
             },
             ..default()
+        })
+        .set(ImagePlugin {
+            default_sampler: ImageSampler::nearest_descriptor(),
         }))
         .insert_resource(ClearColor(Color::BLACK))
-        // .insert_resource(ImageSettings::default_nearest())
         .add_startup_system(setup)
         .add_system(swap_colours)
         .run();
 }
 
-const CONWAYS_MAP_SIZE: Vec2 = Vec2::new(640.0, 640.0);
+const CONWAYS_MAP_SIZE: Vec2 = Vec2::new(160.0, 160.0);
+const CONWAYS_SCREEN_SIZE: Vec2 = Vec2::new(800.0, 800.0);
 
 
 
@@ -30,8 +34,8 @@ struct GameOfLife(HandleId);
 fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
     let mut image = Image::new_fill(
         Extent3d {
-            width: (CONWAYS_MAP_SIZE.x / 4.0) as u32,
-            height: (CONWAYS_MAP_SIZE.y / 4.0) as u32,
+            width: (CONWAYS_MAP_SIZE.x) as u32,
+            height: (CONWAYS_MAP_SIZE.y) as u32,
             depth_or_array_layers: 1,
         },
         TextureDimension::D2,
@@ -41,6 +45,12 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
     image.texture_descriptor.usage =
         TextureUsages::COPY_DST | TextureUsages::STORAGE_BINDING | TextureUsages::TEXTURE_BINDING;
 
+    for (i, x) in image.data.iter_mut().enumerate() {
+        if (i + 1) % 4 == 0 {
+            *x = rand::thread_rng().gen_range(0..=1) * 255;
+        }
+    }
+
     let image = images.add(image.clone());
     let id = image.id();
 
@@ -48,7 +58,7 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
 
     commands.spawn(SpriteBundle {
         sprite: Sprite {
-            custom_size: Some(Vec2::new(CONWAYS_MAP_SIZE.x, CONWAYS_MAP_SIZE.y)),
+            custom_size: Some(CONWAYS_SCREEN_SIZE),
             ..default()
         },
         texture: image,
@@ -62,26 +72,46 @@ fn setup(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
 fn swap_colours (
     mut images: ResMut<Assets<Image>>,
     id: Res<GameOfLife>,
-    keys: Res<Input<KeyCode>>
+    // keys: Res<Input<KeyCode>>
 ) {
-    if keys.just_pressed(KeyCode::Space) {
+    // if keys.just_pressed(KeyCode::Space) {
         let handle = Handle::weak(id.0);
 
         if let Some(image) = images.get_mut(&handle) {
 
-            let new_image: Vec<_> = image.data.clone().iter().enumerate().filter(|(i, _)| (i + 1) % 4 == 0 ).map(|x| 255 - x.1).collect();
+            let mut new_image: Vec<u8> = Vec::new();
 
-            for (i, x) in image.data.clone().iter().enumerate() {
-                if (*x as u32 + 1) % 4 == 0 {
-                    image.data[(*x) as usize] = new_image[i / 4]
+            for _ in 0..(image.data.len() / 4) {
+                new_image.push(0);
+            }
+
+            let old_image: Vec<u8> = image.data.iter().enumerate().filter(|(i, _)| (i + 1) % 4 == 0 ).map(|x| *x.1).collect();
+
+            for (i, x) in old_image.iter().enumerate() {
+
+                let num_neighbors = count_neighbors(&old_image, i.try_into().unwrap());
+
+                if num_neighbors < 2 || num_neighbors > 3 {
+                    new_image[i] = 0
+                } else if num_neighbors == 2 {
+                    new_image[i] = *x
+                } else if num_neighbors == 3 {
+                    new_image[i] = 255;
+                }
+
+            }
+
+            for (i, x) in image.data.iter_mut().enumerate() {
+                if (i + 1) % 4 == 0  {
+                    *x = new_image[((i+1) / 4) - 1];
                 }
             }
         }
-    }
+    // }
 }
 
 fn count_neighbors (
-    image: &Image,
+    image: &Vec<u8>,
     pixel: i32,
 ) -> i32 {
 
@@ -113,10 +143,10 @@ fn count_neighbors (
 
                 let index = (pixel + index) as usize;
 
-                if !index < image.data.len() {
+                if !(index < image.len()) {
                     continue
                 } else {
-                    let number = image.data[index];
+                    let number = image[index];
 
                     if number == 255 {
                         total += 1;
